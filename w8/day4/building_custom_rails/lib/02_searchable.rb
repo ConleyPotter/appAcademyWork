2449db1d1fa8e936c679fea1d08147933bba2a9a
Conley Potter <conley.potter@gmail.com>
2019-04-05 06:16:10 -0500

    Add Thursday's Work

---
+++ w8/day4/building_custom_rails/lib/02_searchable.rb
@@ -0,0 +1,34 @@
+require_relative 'db_connection'
+require_relative '01_sql_object'
+require "byebug"
+
+module Searchable
+  def where(params)
+    where_clause = params.keys.map { |key| "#{key} = ?"}.join(" AND ")
+    where_values = params.values
+    data = DBConnection.instance.execute(<<-SQL, *where_values)
+      SELECT
+        *
+      FROM 
+        #{self.table_name}
+      WHERE
+        #{where_clause}
+    SQL
+
+    array_of_hashes = []
+
+    data.each do |hash|
+      hash_with_symbols = {}
+      hash.each_key do |key|
+        hash_with_symbols[key.to_sym] = data.first[key]
+      end
+      array_of_hashes << hash_with_symbols
+    end
+    
+    array_of_hashes.map { |hash| self.new(hash) }
+  end
+end
+
+class SQLObject
+  extend Searchable
+end
