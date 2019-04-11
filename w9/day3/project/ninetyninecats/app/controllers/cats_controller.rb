b8c6b734066e26bc0055ccc7fb38592e25b2056e
Conley Potter <conley.potter@gmail.com>
2019-04-11 09:06:27 -0500

    Complete Form Partial, New View and Edit View, along with actions in the cat controller

--- w9/day3/project/ninetyninecats/app/controllers/cats_controller.rb
+++ w9/day3/project/ninetyninecats/app/controllers/cats_controller.rb
@@ -8,4 +8,37 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     render :show
   end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    cat = Cat.new(cat_params)
+    if cat.save
+      redirect_to :cats
+    else
+      render json: cat.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def edit 
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      render :show
+    else
+      render json: @cat.errors.full_messages
+    end
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :birthdate, :color, :sex, :description)
+  end
 end
