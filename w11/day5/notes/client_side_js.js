4e6d3f5e6ab03240348a2e095e70785515a68216
Conley Potter <conley.potter@gmail.com>
2019-04-29 08:27:14 -0500

    Monday Commit

---
+++ w11/day5/notes/client_side_js.js
@@ -0,0 +1,26 @@
+// CLIENT-SIDE JAVASCRIPT //
+
+// We've written JavaScript source files for Node.js, but we want to start 
+// writing JavaScript for the browser to execute.
+
+// Let 's start with how to instruct a browser to load and execute JS code:
+
+// <!-- index.html -->
+// <html>
+//   <head>
+//     <title>A page for you!</title>
+//   </head>
+
+//   <body>
+//     <div id="content"></div>
+
+//     <!-- first load the jQuery library hosted by Google -->
+//     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.3/jquery.js"></script>
+
+//     <script>alert("This is inline code!");</script>
+
+//     <!-- then load our own app.js code -->
+//     <script type="application/javascript" src="./app.js"></script>
+//   </body>
+// </html>
+
